47b09ba939ab6d369f540fab4f95fe68ccee354a
kawthermah241@gmail.com <kawthermah241@gmail.com>
2024-01-19 03:29:48 -0800

    Add project files.

---
+++ WindowsFormsApp7/Form2.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalaary = new System.Windows.Forms.TextBox();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dg = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAdd.Location = new System.Drawing.Point(149, 256);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 37);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(149, 91);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(163, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(149, 140);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(163, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtSalaary
+            // 
+            this.txtSalaary.Location = new System.Drawing.Point(149, 186);
+            this.txtSalaary.Name = "txtSalaary";
+            this.txtSalaary.Size = new System.Drawing.Size(163, 20);
+            this.txtSalaary.TabIndex = 3;
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalary.Location = new System.Drawing.Point(40, 185);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(82, 19);
+            this.lblSalary.TabIndex = 6;
+            this.lblSalary.Text = "EmpSalary";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(37, 139);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(79, 19);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "EmpName";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(37, 92);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(57, 19);
+            this.lblID.TabIndex = 4;
+            this.lblID.Text = "EmpID";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnUpdate.Location = new System.Drawing.Point(237, 256);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 37);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnDelete.Location = new System.Drawing.Point(318, 256);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 37);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(452, 40);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(218, 253);
+            this.dg.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(527, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtSalaary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalaary;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dg;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WindowsFormsApp7/Form2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+
+    public partial class Form2 : Form
+    {
+        string data = "Data Source = DESKTOP-IV0INRN\\SQLEXPRESS; Initial Catalog = employee; Integrated Security= True;";
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(data);
+            con.Open();
+            SqlCommand command = new SqlCommand("Insert into information values(@EmpID, @EmpName, @EmpSalary)", con);
+            command.Parameters.AddWithValue("@EmpID", int.Parse(txtId.Text));
+            command.Parameters.AddWithValue("@EmpName", txtId.Text);
+            command.Parameters.AddWithValue("@EmpSalary", int.Parse(txtSalaary.Text));
+            command.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("add successfully");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(data);
+            connection.Open();
+            SqlCommand command = new SqlCommand("Update information Set EmpName=@EmpName, EmpSalary=@EmpSalary Where EmpID=@EmpID", connection);
+            command.Parameters.AddWithValue("@EmpName", txtName.Text);
+            command.Parameters.AddWithValue("@EmpSalary", int.Parse(txtSalaary.Text));
+            command.Parameters.AddWithValue("@EmpID", int.Parse(txtId.Text));
+            command.ExecuteNonQuery ();
+            connection.Close();
+            MessageBox.Show("update is successfully");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(data);
+            SqlCommand command = new SqlCommand("Delete information Where EmpID=@EmpID", connection);
+            command.Parameters.AddWithValue("@EmpID", int.Parse(txtId.Text));
+            connection.Open();
+            command.ExecuteNonQuery ();
+            connection.Close();
+            MessageBox.Show("delete is successfully");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            dg.DataSource = getdata();
+        }
+        DataTable getdata()
+        {
+            SqlConnection connection = new SqlConnection(data);
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from information", connection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+            return dataTable;
+        }
+    }
+}
